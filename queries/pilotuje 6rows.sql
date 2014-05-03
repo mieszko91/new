fc1b2801ebe25163d4f31c9dd01282c23e43ddf6
mieszko91 <mieszko91@op.pl>
2014-05-03 11:35:00 +0200

    nowe zapytania
    nowe zapytania dla rezerwacji i pilotuje pod klientow i rpacownikow Hani

---
+++ queries/pilotuje 6rows.sql
@@ -0,0 +1,25 @@
+INSERT INTO [dbo].[pilotuje]
+           ([Pracownik_Pracownik_ID]
+           ,[Rezerwacja_Rezerwacja_ID])
+     VALUES
+           (4,
+           1),
+		   
+           (5,
+           2),
+		   
+           (5,
+           1),
+		   
+           (5,
+           3),
+		   
+           (4,
+           4),
+		   
+           (6,
+           1)
+
+
+
+
---
+++ queries/rezerwacja 4rows.sql
@@ -0,0 +1,10 @@
+INSERT INTO [dbo].[Rezerwacja]
+           ([Data_wypo¿yczenia]
+           ,[Data_zwrotu]
+           ,[Statek_Statek_ID]
+           ,[Klient_Klient_ID])
+     VALUES
+		   ('2014-05-29', '2014-05-29', 1, 1),
+           ('2014-03-29', '2014-03-29', 3, 3),
+		   ('2014-04-29', '2014-04-29', 4, 2),
+		   ('2014-05-29', '2014-05-29', 2, 4)
